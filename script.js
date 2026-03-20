b09fdcf91a309fbb4cdb4a3547ec9dc6168bdc5f
ooexiaoo <varunpaherwar@proton.me>
2026-03-20 20:46:38 +0530

    updated address

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Initialize Google Map
   const map = document.getElementById('map');
-  map.innerHTML = '<iframe title="map" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3692.4392019643124!2d73.17834087529141!3d22.261346779714568!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDE1JzQwLjkiTiA3M8KwMTAnNTEuMyJF!5e0!3m2!1sen!2sin!4v1742972747019!5m2!1sen!2sin"></iframe>';
+  map.innerHTML = '<iframe title="map" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3692.5250047856694!2d73.06963610000001!3d22.258094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fb86bfde5d08d%3A0xb1ad47a816576045!2sUbhu%20Faliyu%2C%20115%2C%20near%20Ramji%20Mandir%2C%20Tajpura%2C%20Gujarat%20391440!5e0!3m2!1sen!2sin!4v1774019102405!5m2!1sen!2sin"></iframe>';
   
   // Mobile Navigation Toggle
   const hamburger = document.querySelector('.hamburger');
